1dcb61c7e36a5ea28f935c2ede641ab919dbc442
bornreddy <marisa.reddy@gmail.com>
2014-07-28 13:46:00 -0400

    switched step one and step two, declaring replace functions before searching for tweets

--- bot.py
+++ bot.py
@@ -1,28 +1,12 @@
 import credentials as cred
 import tweepy
+import sys
 
 auth = tweepy.OAuthHandler(cred.consumer_key, cred.consumer_secret)
 auth.set_access_token(cred.access_token_key, cred.access_token_secret)
 api = tweepy.API(auth)
 
-#Step one: search Twitter for relevant tweets.
-
-matching_tweets = api.search("fuck")
-print "number of returned tweets", len(matching_tweets)
-
-print matching_tweets[0].user
-
-for tweet in matching_tweets:
-    try:
-       print "original tweet: " + tweet.text
-       print "censored tweet: " +  tweet_formatting(tweet.text)
-    except:
-        print "strange characters in tweet preventing the text from printing"
-    print "-------------------------------------\n\n\n"
-
-
-
-#Step two: replace "fuck" with "duck."
+#Step one: replace "fuck" with "duck."
 def word_replace(word):
     if 'Fuck' in word:
         word_remainder = word.split('Fuck')
@@ -41,5 +25,23 @@ def word_replace(word):
 
 def tweet_formatting(tweet):
     return ' '.join(map(word_replace, tweet.split(' ')))
+
+#Step two: search Twitter for relevant tweets.
+
+# make a method to check if the body of the tweet contains query string
+# make a method to check if the body of the tweet can be printed
+
+matching_tweets = api.search("fuck")
+
+for tweet in matching_tweets:
+    try:
+       print "original tweet: " + tweet.text
+       print "censored tweet: " +  tweet_formatting(tweet.text)
+    except:
+        print "strange characters in tweet preventing the text from printing"
+    print "-------------------------------------\n\n\n"
+
+
+
 
 #Step three: RT.

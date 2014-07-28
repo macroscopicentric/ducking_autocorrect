1e5f693a445b2a406de257c41bbc6b8a697329ae
Rachel King <rachel.b.king@gmail.com>
2014-07-28 14:29:58 -0400

    Added main().

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import credentials as cred
 import tweepy
 import re
+import time
 
 auth = tweepy.OAuthHandler(cred.consumer_key, cred.consumer_secret)
 auth.set_access_token(cred.access_token_key, cred.access_token_secret)
@@ -27,8 +28,8 @@ test1 = 'I fucking hate autocorrect.'
 test2 = 'Fuck you.'
 test3 = 'FUcK YOU'
 
-def letter_replace(word):
-    word = word.groups()[0]
+def letter_replace(regex_object):
+    word = regex_object.groups()[0]
     if word[0].isupper():
         word = 'D' + word[1:]
     else:
@@ -43,4 +44,12 @@ def tweet_formatting(tweet_text):
 def retweet(tweet):
     #Change from return to actual tweepy retweet method when we're done.
     return 'rt @' + tweet.screen_name + tweet_formatting(tweet.text)
+
+def main():
+    print retweet(search_tweets())
+    time.sleep(600)
+
+if __name__ == '__main__':
+    while True:
+        main()
 

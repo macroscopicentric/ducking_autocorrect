d79fc9feb5861e2c9e21b3d683491c8945ac914a
Rachel King <rachel.b.king@gmail.com>
2014-07-31 20:25:18 -0400

    Yay typos.

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import re
 import time
 import random
 
-auth = tweepy.OAuthHandler(os.environ['consumer_key'], os.environ['cred.consumer_secret'])
+auth = tweepy.OAuthHandler(os.environ['consumer_key'], os.environ['consumer_secret'])
 auth.set_access_token(os.environ['access_token_key'], os.environ['access_token_secret'])
 api = tweepy.API(auth)
 
